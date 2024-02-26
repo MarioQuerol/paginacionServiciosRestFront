69731cd25a52a81af67944fee151a6ea90360fad
Mario Querol <mario@devanddel.com>
2024-02-26 12:54:31 +0100

    Added material

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -2,17 +2,18 @@ import { Pagination } from '../../models/pagination-mode';
 import { Product } from '../../models/product-model';
 import { ShopService } from './../../services/shop.service';
 import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
-
+  displayedColumns: string[] = ['id', 'nombre', 'precio'];
   constructor(private shopservice: ShopService) {}
 
   ngOnInit(): void {
